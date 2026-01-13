6b794785e4d7fe9f1eca98cb6444d97a16206118
kalmk <111930167+kalmk@users.noreply.github.com>
2026-01-12 17:10:37 -0800

    fixed usagi timestamp plus added lyrics

--- frontend/src/data/songs.js
+++ frontend/src/data/songs.js
@@ -5,11 +5,12 @@ export const songs = [
     name: "Rabbit",
     audioSrc: "/assets/Rabbit_xf.mp3",
     lyrics: [
-      { text: "First line of lyrics", start: 0, end: 8.5 },
-      { text: "Second line of lyrics", start: 8.5, end: 11.5 },
-      { text: "Third line of lyrics", start: 11.5, end: 14.5 },
-      { text: "Fourth line of lyrics", start: 14.5, end: 22 },
-      { text: "Fifth line of lyrics", start: 22, end: 30 },
+      { text: "...", start: 0, end: 8.5 },
+      { text: "うさぎ うさぎ", start: 8.5, end: 11.5 },
+      { text: "なにみてはねる", start: 11.5, end: 14.5 },
+      { text: "じゅうごやおつきさま", start: 14.5, end: 17.5 },
+      { text: "みてはねる", start: 17.5, end: 22 },
+      { text: "...", start: 22, end: 30 },
     ],
   },
   // you can add more songs here later
