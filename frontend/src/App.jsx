f40e81e7a5c1bc305cc20a6548119eb67e60bfe2
kalmk <111930167+kalmk@users.noreply.github.com>
2026-01-17 16:51:23 -0800

    added pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import SongSelect from "./pages/SongSelect";
 import Game from "./pages/Game";
+import Welcome from "./pages/Welcome";
 
 export default function App() {
   return (
     <Routes>
+      {/* Implement all path to the pages accordingly */}
+      <Route path="/welcome" element={<Welcome />} />
+
+      {/* Gameplay */}
       <Route path="/songs" element={<SongSelect />} />
       <Route path="/play/:songId" element={<Game />} />
     </Routes>
