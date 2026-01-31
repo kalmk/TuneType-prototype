280d3796547f177a55f395ecfdcd52b634de55db
nwindly <nwindly@gmail.com>
2026-01-30 21:03:01 -0800

    signin updates
    - added a path to the signin.jsx
    - edits to the SignIn page
    - installed toastify for a browser modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Game from "./pages/Game";
 import Welcome from "./pages/Welcome";
 import HomePage from "./pages/HomePage";
 import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
   return (
@@ -12,6 +14,8 @@ export default function App() {
       <Route path="/" element={<Welcome />} />
       <Route path="/homepage" element={<HomePage />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
 
       {/* Gameplay */}
       <Route path="/songs" element={<SongSelect />} />
