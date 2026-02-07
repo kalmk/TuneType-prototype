d761e075744c682ead80350f798ea3cb8c561ded
kalmk <111930167+kalmk@users.noreply.github.com>
2026-02-07 13:12:23 -0800

    setup mongodb

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,1 +1,12 @@
 // backend/src/config/db.js
+
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("MongoDB connection error:", error);
+  }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,2 +1,15 @@
 // backend/src/index.js
-console.log("Hello world")
+
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+  connectDB();
+});
