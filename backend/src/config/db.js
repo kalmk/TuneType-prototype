42331dc92725642209591ec0761e16fee8893825
nwindly <nwindly@gmail.com>
2026-02-07 13:29:39 -0800

    backend set up

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,1 +1,12 @@
 // backend/src/config/db.js
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB connected successfully: ${conn.connection.host}');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1); // Exit with failure
+    }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,2 +1,13 @@
+import dotenv from 'dotenv';
+import { connectDB } from './config/db';
 // backend/src/index.js
+
+dotenv.config()
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)});
+    connectDB()
+
 console.log("Hello world")
